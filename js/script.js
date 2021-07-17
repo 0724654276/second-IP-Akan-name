d31c2cd3a6c625a24932117bcf8e2deae1441caa
0724654276 <george.macharia@students.moringaschool.com>
2021-07-17 12:24:28 +0300

    commit adds formulae

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@ function getAkanName() {
     var mm = parseInt(month);
     var year = document.getElementById("year").Value;
     var yy = parseInt(year);
+    var cc =Math.ceil(year / 100)
+    // using the formulae
+    var dayOFweek(d) = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
     
+
 
 
 
