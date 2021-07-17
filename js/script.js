c62e49f9937f9fc45e7085d4218ca4b6cf72d90f
0724654276 <george.macharia@students.moringaschool.com>
2021-07-17 12:07:50 +0300

    commit declares variables

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function getAkanName() {
+    // declariaring variables
+    var day = document.getElementById("day").Value;
+    var dd = parseInt(day);
+    var month = document.getElementById("month").Value;
+    var mm = parseInt(month);
+    var year = document.getElementById("year").Value;
+    var yy = parseInt(year);
+    
+
+
+
+
+
+
+}
